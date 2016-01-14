e3f27308e881daa0785edb31d183b9eae1328719
litwrd <litwrd@163.com>
2016-01-14 15:46:07 +0800

    update stock dictionary project

--- data_stock/dic/StockDic.dpr
+++ data_stock/dic/StockDic.dpr
@@ -2,8 +2,14 @@ program StockDic;
 
 uses
   Forms,
-  ALQuickList_int in '..\..\..\litwrd_git\RefComponents\AllInOne\ALQuickList_int.pas',
-  ALQuickSortList in '..\..\..\litwrd_git\RefComponents\AllInOne\ALQuickSortList.pas',
+  QuickList_Int in '..\..\..\devwintech\comps\list\QuickList_Int.pas',
+  QuickSortList in '..\..\..\devwintech\comps\list\QuickSortList.pas',
+  BaseDataSet in '..\..\..\devwintech\v0000\win_base\BaseDataSet.pas',
+  BaseWinApp in '..\..\..\devwintech\v0000\win_base\BaseWinApp.pas',
+  BasePath in '..\..\..\devwintech\v0000\win_base\BasePath.pas',
+  BaseApp in '..\..\..\devwintech\v0000\win_base\BaseApp.pas',
+  BaseWinFile in '..\..\..\devwintech\v0000\win_base\BaseWinFile.pas',
+  BaseFile in '..\..\..\devwintech\v0000\win_base\BaseFile.pas',
   DefineSys in '..\..\..\litwrd_git\AppPrestudy\Windows\DealData\v03\DefineBase\DefineSys.pas',
   Define_StockItem in '..\..\..\litwrd_git\AppPrestudy\Windows\DealData\v03\Define\Define_StockItem.pas',
   Define_Price in '..\..\basedefine\Define_Price.pas',
@@ -14,13 +20,7 @@ uses
   DB_StockItem_Save in '..\..\..\litwrd_git\AppPrestudy\Windows\DealData\v03\StockItem\DB_StockItem_Save.pas',
   DB_StockItem_LoadIni in '..\..\..\litwrd_git\AppPrestudy\Windows\DealData\v03\StockItem\DB_StockItem_LoadIni.pas',
   DB_StockItem_Load in '..\..\..\litwrd_git\AppPrestudy\Windows\DealData\v03\StockItem\DB_StockItem_Load.pas',
-  StockDicForm in 'StockDicForm.pas' {frmStockDic},
-  BaseDataSet in '..\..\..\devwintech\v0000\win_base\BaseDataSet.pas',
-  BaseWinApp in '..\..\..\devwintech\v0000\win_base\BaseWinApp.pas',
-  BasePath in '..\..\..\devwintech\v0000\win_base\BasePath.pas',
-  BaseApp in '..\..\..\devwintech\v0000\win_base\BaseApp.pas',
-  BaseWinFile in '..\..\..\devwintech\v0000\win_base\BaseWinFile.pas',
-  BaseFile in '..\..\..\devwintech\v0000\win_base\BaseFile.pas';
+  StockDicForm in 'StockDicForm.pas' {frmStockDic};
 
 {$R *.res}
 
