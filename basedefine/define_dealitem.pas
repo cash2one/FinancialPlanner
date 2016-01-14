0e87748378b45a3d3910b64f026a0ac07d05a88f
litwrd <litwrd@163.com>
2016-01-14 16:23:53 +0800

    add define deal item

---
+++ basedefine/define_dealitem.pas
@@ -0,0 +1,78 @@
+unit define_dealitem;
+
+interface
+                   
+uses
+  Define_Price;
+              
+type            
+  TDealCodePack = integer;
+
+  PRT_DealItem          = ^TRT_DealItem;
+  TRT_DealItem          = packed record
+    Country             : Word;
+    //MarketCode          : array[0..3] of AnsiChar; 
+    sMarketCode         : AnsiString;
+    iMarketCode         : integer;    
+    //Code                : array[0..7] of AnsiChar;   
+    sCode               : AnsiString;
+    iCode               : TDealCodePack;    
+    //Name                : array[0..5] of WideChar;
+    Name                : AnsiString;
+    //ItemFlag            : Byte;
+    IsDataChange        : Byte;
+    FirstDealDate       : Word;          // 2
+    LastDealDate        : Word;          // 2
+    EndDealDate         : Word;          // 2
+  end;
+         
+  PStore_DealItem32     = ^TStore_DealItem32;
+  TStore_DealItem32     = packed record // 128
+    Code                : array[0..12 - 1] of AnsiChar; // 12   
+    Name                : array[0..6 - 1] of WideChar; // 12    
+    Country             : Word; // 2 26
+    FirstDealDate       : Word; // 2 28
+    EndDate             : Word; // 2 30
+  end;
+
+  PStore_DealItem32Rec  = ^TStore_DealItem32Rec;
+  TStore_DealItem32Rec  = packed record
+    ItemRecord          : TStore_DealItem32;
+    Reserve             : array[0..32 - 1 - SizeOf(TStore_DealItem32)] of Byte;
+  end;
+
+  function getStockCodePack(AStockCode: string) : integer;
+  function getStockCodeByPackCode(AStockCode: integer) : string;
+    
+implementation
+
+uses
+  SysUtils, Define_String;
+  
+function getStockCodePack(AStockCode: AnsiString) : integer;
+begin
+  Result := 0;                   
+  if 6 < Length(AStockCode) then
+    AStockCode := Copy(AStockCode, Length(AStockCode) - 6 + 1, maxint);
+  if 6 = Length(AStockCode) then
+  begin
+    if '6' = AStockCode[FirstStringIndex] then
+    begin
+      Result := StrToIntDef(AStockCode, 0);
+    end else
+    begin
+      Result := StrToIntDef('1' + AStockCode, 0);
+    end;
+  end;
+end;
+
+function getStockCodeByPackCode(AStockCode: integer) : AnsiString;
+begin
+  Result := IntToStr(AStockCode);
+  if 6 < Length(Result) then
+  begin
+    Result := Copy(Result, Length(Result) - 6 + 1, maxint);
+  end;
+end;
+
+end.
--- win_dealhelper_zszq/zsHelper.dpr
+++ win_dealhelper_zszq/zsHelper.dpr
@@ -11,12 +11,6 @@ uses
   Define_Message in '..\..\devwintech\v0000\win_basedefine\Define_Message.pas',
   UtilsWindows in '..\..\devwintech\v0000\win_utils\UtilsWindows.pas',
   UtilsApplication in '..\..\devwintech\v0000\win_utils\UtilsApplication.pas',
-  
-  Define_StockItem in '..\..\litwrd_git\AppPrestudy\Windows\DealData\v03\Define\Define_StockItem.pas',
-  Define_Store_Header in '..\..\litwrd_git\AppPrestudy\Windows\DealData\v03\Define\Define_Store_Header.pas',
-  AppWindow in '..\..\litwrd_git\AppStock\base\AppWindow.pas',
-  CmdWindow in '..\..\litwrd_git\AppStock\base\CmdWindow.pas',
-
   define_price in '..\basedefine\define_price.pas',
   zsVariants in 'zsVariants.pas',
   zsLoginUtils in 'zsLoginUtils.pas',
