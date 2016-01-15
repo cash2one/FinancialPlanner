23709a3e50984f80fa250178f4d74b7261be2d04
litwrd <litwrd@163.com>
2016-01-15 15:10:52 +0800

    add stock day sina app

---
+++ win_app_data/datasrc_sina/StockDay_Sina.dpr
@@ -0,0 +1,74 @@
+﻿program StockDay_Sina;
+
+uses
+  Windows,                                                      
+  QuickSortList in '..\..\..\devwintech\comps\list\QuickSortList.pas',
+  QuickList_Int in '..\..\..\devwintech\comps\list\QuickList_Int.pas',
+  BaseWinApp in '..\..\..\devwintech\v0000\win_base\BaseWinApp.pas',
+  BasePath in '..\..\..\devwintech\v0000\win_base\BasePath.pas',
+  BaseFile in '..\..\..\devwintech\v0000\win_base\BaseFile.pas',
+  BaseDataSet in '..\..\..\devwintech\v0000\win_base\BaseDataSet.pas',
+  BaseApp in '..\..\..\devwintech\v0000\win_base\BaseApp.pas',
+  BaseWinThread in '..\..\..\devwintech\v0000\win_base\BaseWinThread.pas',
+  BaseWinFile in '..\..\..\devwintech\v0000\win_base\BaseWinFile.pas',   
+  Define_String in '..\..\..\devwintech\v0000\win_basedefine\Define_String.pas',      
+  UtilsHttp in '..\..\..\devwintech\v0000\win_utils\UtilsHttp.pas',
+  UtilsHttp_Indy in '..\..\..\devwintech\v0000\win_utils\UtilsHttp_Indy.pas',
+  UtilsHtmlParser in '..\..\..\devwintech\v0000\win_utils\UtilsHtmlParser.pas',
+  UtilsDateTime in '..\..\..\devwintech\v0000\win_utils\UtilsDateTime.pas',
+            
+  BaseStockApp in '..\..\base\BaseStockApp.pas',
+  StockAppPath in '..\..\base\StockAppPath.pas',
+
+  define_dealmarket in '..\..\basedefine\define_dealmarket.pas',
+  define_dealitem in '..\..\basedefine\define_dealitem.pas',
+  define_datasrc in '..\..\basedefine\define_datasrc.pas',
+  define_price in '..\..\basedefine\define_price.pas',
+  define_dealstore_header in '..\..\basedefine\define_dealstore_header.pas',
+  define_dealstore_file in '..\..\basedefine\define_dealstore_file.pas',
+  define_stock_quotes in '..\..\basedefine\define_stock_quotes.pas',
+
+  db_dealitem in '..\..\dealitem\db_dealitem.pas',
+  db_dealitem_load in '..\..\dealitem\db_dealitem_load.pas',
+  db_dealitem_save in '..\..\dealitem\db_dealitem_save.pas',
+
+  StockDayDataAccess in '..\..\data_stock\StockDayDataAccess.pas',
+  StockDayData_Load in '..\..\data_stock\StockDayData_Load.pas',
+  StockDayData_Save in '..\..\data_stock\StockDayData_Save.pas',
+  
+  StockDay_Get_Sina in 'StockDay_Get_Sina.pas';
+
+{$R *.res}
+
+type
+  TStockDaySinaApp = class(TBaseStockApp)
+  protected
+  public     
+    constructor Create(AppClassId: AnsiString); override;
+    procedure Run; override;
+  end;
+
+{ TStockDay163App }
+
+constructor TStockDaySinaApp.Create(AppClassId: AnsiString);
+begin
+  inherited;
+end;
+
+procedure TStockDaySinaApp.Run;
+begin
+  GetStockDataDay_Sina_All(Self);
+end;
+
+var
+  GlobalApp: TStockDaySinaApp;
+begin
+  GlobalApp := TStockDaySinaApp.Create('');
+  try
+    if GlobalApp.Initialize then
+      GlobalApp.Run;
+    GlobalApp.Finalize;
+  finally
+    GlobalApp.Free;
+  end;
+end.
