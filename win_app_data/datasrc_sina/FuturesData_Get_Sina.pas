37373bb88fa464431c0c80efd4955fb4e5e1d17b
litwrd <litwrd@163.com>
2016-01-19 19:04:30 +0800

    add futures data url

--- win_app_data/datasrc_sina/FuturesData_Get_Sina.pas
+++ win_app_data/datasrc_sina/FuturesData_Get_Sina.pas
@@ -25,6 +25,14 @@ uses
   DB_DealItem_Load,
   DB_DealItem_Save;
 
+const            
+  BaseSinaFuturesInstantDataUrl = 'http://hq.sinajs.cn/list=';  //http://hq.sinajs.cn/list=CFF_RE_IF1603
+  BaseSinaFuturesData5MinUrl = 'http://stock2.finance.sina.com.cn/futures/api/json.php/CffexFuturesService.getCffexFuturesMiniKLine5m?symbol='; // IF1603
+  BaseSinaFuturesData15MinUrl = 'http://stock2.finance.sina.com.cn/futures/api/json.php/CffexFuturesService.getCffexFuturesMiniKLine15m?symbol=';// IF1606
+  BaseSinaFuturesData30MinUrl = 'http://stock2.finance.sina.com.cn/futures/api/json.php/CffexFuturesService.getCffexFuturesMiniKLine30m?symbol=';// IF1606  
+  BaseSinaFuturesData60MinUrl = 'http://stock2.finance.sina.com.cn/futures/api/json.php/CffexFuturesService.getCffexFuturesMiniKLine60m?symbol=';// IF1606
+  BaseSinaFuturesDayUrl = 'http://stock2.finance.sina.com.cn/futures/api/json.php/CffexFuturesService.getCffexFuturesDailyKLine?symbol=';//IF1606
+
 var
   DateFormat_Sina: Sysutils.TFormatSettings;(*// =(
     CurrencyString: '';
