e29f5ff50eee100cb64b6f923b06a766d221f2ba
litwrd <litwrd@163.com>
2016-05-29 16:51:15 +0800

    add stock detail pack

---
+++ win_app_data/data_detail/StockDetail_Pack.dpr
@@ -0,0 +1,66 @@
+﻿program StockDetail_Pack;
+
+uses
+  Windows,
+  QuickSortList in '..\..\..\devwintech\comps\list\QuickSortList.pas',
+  QuickList_Int in '..\..\..\devwintech\comps\list\QuickList_Int.pas',
+  WinSock2 in '..\..\..\devwintech\common\WinSock2.pas',
+  Base.Run in '..\..\..\devwintech\v0001\rec\app_base\Base.Run.pas',
+  BaseApp in '..\..\..\devwintech\v0001\obj\app_base\BaseApp.pas',
+  BasePath in '..\..\..\devwintech\v0001\obj\app_base\BasePath.pas',
+  BaseFile in '..\..\..\devwintech\v0001\obj\app_base\BaseFile.pas',
+  BaseThread in '..\..\..\devwintech\v0001\obj\app_base\BaseThread.pas',
+  BaseWinApp in '..\..\..\devwintech\v0001\obj\win_app\BaseWinApp.pas',
+  sysdef_string in '..\..\..\devwintech\v0001\sysdef\sysdef_string.pas',
+  BaseDataSet in '..\..\..\devwintech\v0000\app_base\BaseDataSet.pas',
+  BaseWinFile in '..\..\..\devwintech\v0000\win_base\BaseWinFile.pas',
+  win.iobuffer in '..\..\..\devwintech\v0000\win_data\win.iobuffer.pas',
+  UtilsDateTime in '..\..\..\devwintech\v0000\win_utils\UtilsDateTime.pas',
+  BaseStockApp in '..\..\base\BaseStockApp.pas',
+  StockAppPath in '..\..\base\StockAppPath.pas',
+  define_dealmarket in '..\..\basedefine\define_dealmarket.pas',
+  define_dealitem in '..\..\basedefine\define_dealitem.pas',
+  define_datasrc in '..\..\basedefine\define_datasrc.pas',
+  define_price in '..\..\basedefine\define_price.pas',
+  define_dealstore_header in '..\..\basedefine\define_dealstore_header.pas',
+  define_dealstore_file in '..\..\basedefine\define_dealstore_file.pas',
+  define_stock_quotes in '..\..\basedefine\define_stock_quotes.pas',
+  db_dealitem in '..\..\dealitem\db_dealitem.pas',
+  db_dealitem_load in '..\..\dealitem\db_dealitem_load.pas',
+  db_dealitem_save in '..\..\dealitem\db_dealitem_save.pas',
+  StockDayDataAccess in '..\..\data_stock\StockDayDataAccess.pas',
+  StockDayData_Load in '..\..\data_stock\StockDayData_Load.pas';
+
+{$R *.res}
+
+type
+  TStockDetailPackApp = class(TBaseStockApp)
+  protected
+  public     
+    constructor Create(AppClassId: AnsiString); override;
+    procedure Run; override;
+  end;
+
+{ TStockDay163App }
+
+constructor TStockDetailPackApp.Create(AppClassId: AnsiString);
+begin
+  inherited;
+end;
+
+procedure TStockDetailPackApp.Run;
+begin
+end;
+
+var
+  GlobalApp: TStockDetailPackApp;
+begin
+  GlobalApp := TStockDetailPackApp.Create('');
+  try
+    if GlobalApp.Initialize then
+      GlobalApp.Run;
+    GlobalApp.Finalize;
+  finally
+    GlobalApp.Free;
+  end;
+end.
