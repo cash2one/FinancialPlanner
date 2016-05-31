c3340ed1a39ec25e2bcc5a4f308b05ebe53bf6bf
litwrd <litwrd@github.com>
2016-05-31 14:18:55 +0800

    update stock detail pack

--- win_app_data/data_detail/StockDetail_PackApp.pas
+++ win_app_data/data_detail/StockDetail_PackApp.pas
@@ -20,6 +20,7 @@ uses
   Sysutils,    
   define_dealitem,
   define_datasrc,
+  define_stock_quotes,
   define_dealstore_file,       
   DB_DealItem,
   DB_DealItem_Load,
@@ -28,6 +29,7 @@ uses
   StockDayDataAccess,
   StockDayData_Load,
   StockDetailDataAccess,
+  StockDetailData_Save,
   StockDetailData_Load;
 
 { TStockDay163App }
@@ -43,6 +45,8 @@ type
     IsReady: Integer;
     Year: Word;
     Month: Word;
+    DataSrcId: Integer;
+    StockItem: PRT_DealItem;
     StockDetails: TALIntegerList;
   end;
   
@@ -64,16 +68,50 @@ begin
   end;
 end;
                 
-procedure PackStockDetailPack_Month(AStockDetailPack_Month: PStockDetailPack_Month);
+procedure PackStockDetailPack_Month(App: TBaseApp; AStockDetailPack_Month: PStockDetailPack_Month);
+var
+  i, j: integer;                
+  tmpMonthDetailData: TStockDetailDataAccess;   
+  tmpDayDetailData: TStockDetailDataAccess;
+  tmpDayDetailRec: PRT_Quote_M2;
+  tmpMonthDetailRec: PRT_Quote_M2;        
 begin
-  Log('PackStockDetailPack_Month', '');    
   if 0 = AStockDetailPack_Month.Year then
     exit;
   if 0 = AStockDetailPack_Month.Month then
-    exit;
+    exit;                           
+  if 0 = AStockDetailPack_Month.IsReady then
+  begin
+    if nil <> AStockDetailPack_Month.StockDetails then
+    begin               
+      Log('StockDetail_PackApp.pas', 'PackStockDetailPack_Month' + AStockDetailPack_Month.StockItem.sCode + ':' +
+         IntToStr(AStockDetailPack_Month.Year) + '/' + IntToStr(AStockDetailPack_Month.Month));  
+      AStockDetailPack_Month.StockDetails.Sort;
+      tmpMonthDetailData := TStockDetailDataAccess.Create(AStockDetailPack_Month.StockItem, AStockDetailPack_Month.DataSrcId);
+      try
+        for i := 0 to AStockDetailPack_Month.StockDetails.Count - 1 do
+        begin
+          tmpDayDetailData := TStockDetailDataAccess(AStockDetailPack_Month.StockDetails.Objects[i]);
+          if 0 = tmpMonthDetailData.FirstDealDate then
+            tmpMonthDetailData.FirstDealDate := tmpDayDetailData.FirstDealDate;
+          tmpMonthDetailData.LastDealDate := tmpDayDetailData.FirstDealDate;
+          for j := 0 to tmpDayDetailData.RecordCount - 1 do
+          begin
+            tmpDayDetailRec := tmpDayDetailData.RecordItem[j];
+            
+            tmpMonthDetailRec := tmpMonthDetailData.NewRecord(tmpDayDetailRec.DealDateTime.DateValue, tmpDayDetailRec.DealDateTime.TimeValue);
+            tmpMonthDetailRec^ := tmpDayDetailRec^; 
+          end;
+        end;     
+        //SaveStockDetailData(App, tmpMonthDetailData);
+      finally
+        tmpMonthDetailData.Free;
+      end;
+    end;
+  end;
 end;
 
-procedure StockDetailPack_Item(App: TBaseApp; AStockDayAccess: TStockDayDataAccess); overload;
+procedure StockDetailPack_Item(App: TBaseApp; AStockDayAccess: TStockDayDataAccess; ADataSrcId: Integer); overload;
 var
   i, j: integer;  
   tmpYear, tmpMonth, tmpDay: Word;
@@ -85,6 +123,8 @@ var
   tmpFileUrl: string;
 begin
   FillChar(tmpDetailPackData, SizeOf(tmpDetailPackData), 0);
+  tmpDetailPackData.StockItem := AStockDayAccess.StockItem;
+  tmpDetailPackData.DataSrcId := ADataSrcId; 
   tmpIsNew := false;
   for i := AStockDayAccess.LastDealDate downto AStockDayAccess.FirstDealDate do
   begin
@@ -95,38 +135,61 @@ begin
       tmpIsNew := true;
     if tmpIsNew then
     begin
-      PackStockDetailPack_Month(@tmpDetailPackData);
+      PackStockDetailPack_Month(App, @tmpDetailPackData);
       tmpDetailPackData.IsReady := 0;
       tmpDetailPackData.Year := tmpYear;
       tmpDetailPackData.Month := tmpMonth;
+      
+      DecodeDate(now, tmpYear, tmpMonth, tmpDay);
+
+      if (tmpDetailPackData.Year = tmpYear) and
+         (tmpDetailPackData.Month = tmpMonth) then
+      begin
+        tmpDetailPackData.IsReady := 1;
+      end;
+
       tmpIsNew := false;
       if nil = tmpDetailPackData.StockDetails then
         tmpDetailPackData.StockDetails := TALIntegerList.Create;
       ClearStockDetailPack_Month(@tmpDetailPackData);
     end;      
-    if 2016 > tmpYear then
+    if 2016 > tmpDetailPackData.Year then
     begin
       Break;
     end;
-
-    tmpFilePathYear := App.Path.GetFilePath(FilePath_DBType_DetailData, DataSrc_Sina, i, AStockDayAccess.StockItem);
-    tmpFileName := App.Path.GetFileName(FilePath_DBType_DetailData, DataSrc_Sina, i, AStockDayAccess.StockItem, '');
-
-    if '' <> tmpFileName then
+    if 0 = tmpDetailPackData.IsReady then
     begin
-      tmpFileUrl := tmpFilePathYear + tmpFileName;
-      if not FileExists(tmpFileUrl) then
-        tmpFileUrl := ChangeFileExt(tmpFileUrl, '.sdet');
-      if FileExists(tmpFileUrl) then
+      tmpFilePathYear := App.Path.GetFilePath(FilePath_DBType_DetailData, ADataSrcId, i, AStockDayAccess.StockItem);
+      tmpFileName := App.Path.GetFileName(FilePath_DBType_DetailData, ADataSrcId, i, AStockDayAccess.StockItem, '');
+      if '' <> tmpFileName then
       begin
-        tmpDetailData := TStockDetailDataAccess.Create(AStockDayAccess.StockItem, DataSrc_Sina);
-        LoadStockDetailData(App, tmpDetailData, tmpFileUrl);
+        tmpFileUrl := tmpFilePathYear + tmpFileName;
+        if not FileExists(tmpFileUrl) then
+          tmpFileUrl := ChangeFileExt(tmpFileUrl, '.sdet');
+        if FileExists(tmpFileUrl) then
+        begin
+          tmpDetailData := TStockDetailDataAccess.Create(AStockDayAccess.StockItem, ADataSrcId);
+          tmpDetailPackData.StockDetails.AddObject(i, tmpDetailData);
+          tmpDetailData.FirstDealDate := i;
+          tmpDetailData.LastDealDate := i;          
+          LoadStockDetailData(App, tmpDetailData, tmpFileUrl);
+          if 1 > tmpDetailData.RecordCount then
+          begin
+            tmpDetailPackData.IsReady := 1;
+          end else
+          begin
+            Log('StockDetail_PackApp.pas', 'Load Detail Data Error Data Empty:' + AStockDayAccess.StockItem.sCode + ':' + FormatDateTime('yyyy-mm-dd', i));
+          end;
+        end else
+        begin
+          tmpDetailPackData.IsReady := 1;
+        end;
       end;
     end;
   end;
 end;
 
-procedure StockDetailPack_Item(App: TBaseApp; AStockItem: PRT_DealItem); overload;
+procedure StockDetailPack_Item(App: TBaseApp; AStockItem: PRT_DealItem; ADataSrcId: integer); overload;
 var
   tmpDayData: TStockDayDataAccess;
 begin
@@ -134,7 +197,7 @@ begin
   try
     if LoadStockDayData(App, tmpDayData) then
     begin
-      StockDetailPack_Item(App, tmpDayData);
+      StockDetailPack_Item(App, tmpDayData, ADataSrcId);
     end;
   finally
     tmpDayData.Free;
@@ -153,7 +216,8 @@ begin
     begin
       if 0 = tmpDBStockItem.Items[i].EndDealDate then
       begin
-        StockDetailPack_Item(Self, tmpDBStockItem.Items[i]);
+        StockDetailPack_Item(Self, tmpDBStockItem.Items[i], DataSrc_Sina);
+        Break;
       end;
     end;
   finally
