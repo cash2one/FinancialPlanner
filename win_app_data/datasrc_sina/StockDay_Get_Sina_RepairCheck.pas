07b7c253c06ecd552af0ce09c3ffb4ae9577f3e1
litwrd <litwrd@github.com>
2016-06-12 15:39:45 +0800

    add check stock repair

---
+++ win_app_data/datasrc_sina/StockDay_Get_Sina_RepairCheck.pas
@@ -0,0 +1,62 @@
+unit StockDay_Get_Sina_RepairCheck;
+
+interface
+
+uses
+  BaseApp;
+
+  procedure CheckStockDataDay_Sina_All_Repair(App: TBaseApp; AIsWeight: Boolean);
+
+implementation
+
+uses
+  Windows,
+  Sysutils,
+  Classes,
+  UtilsHttp,
+  Define_Price,
+  Define_DealItem,
+  Define_DataSrc,    
+  define_stock_quotes,
+  StockDayData_Get_Sina_RepairCheck,     
+  UtilsDateTime,   
+  //UtilsLog,         
+  StockDayDataAccess,
+  StockDayData_Load,
+  DB_DealItem,
+  DB_DealItem_Load;                 
+                    
+
+procedure CheckStockDataDay_Sina_All_Repair(App: TBaseApp; AIsWeight: Boolean);
+var
+  tmpDBStockItem: TDBDealItem;
+  tmpNetClientSession: THttpClientSession;
+  i: integer;
+  tmpDealItem: PRT_DealItem;
+  tmpRepeat: Integer;
+begin
+  FillChar(tmpNetClientSession, SizeOf(tmpNetClientSession), 0);
+  tmpNetClientSession.IsKeepAlive := true;
+  tmpNetClientSession.ConnectionSession.ConnectTimeOut := 5000;
+  tmpNetClientSession.ConnectionSession.ReceiveTimeOut := 5000;
+  tmpNetClientSession.ConnectionSession.SendTimeOut := 1000;
+
+  tmpDBStockItem := TDBDealItem.Create;
+  try
+    LoadDBStockItemDic(App, tmpDBStockItem);
+    for i := 0 to tmpDBStockItem.RecordCount - 1 do
+    begin
+      tmpDealItem := tmpDBStockItem.Items[i];
+      if 0 = tmpDealItem.EndDealDate then
+      begin
+        if CheckStockDataDay_Sina_Repair(App, tmpDealItem, AIsWeight, @tmpNetClientSession) then
+        begin
+        end;
+      end;
+    end;
+  finally
+    tmpDBStockItem.Free;
+  end;
+end;
+
+end.
